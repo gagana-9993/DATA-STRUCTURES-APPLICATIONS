a833d93a40b336341251a3b1aaca1211216068b9
gagana-9993 <gagana9993@gmail.com>
2025-10-23 14:01:23 +0530

    queue.c

---
+++ queue.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 5
+struct queue
+{
+    int r,f;
+    int data[SIZE];
+};
+typedef struct queue QUEUE;
+void enqueue(QUEUE *q,int item)
+{
+    if(q->r==SIZE-1)
+        printf("\n queue full");
+    else
+    {
+        q->r=q->r+1;
+        q->data[q->r]=item;
+        if(q->f==-1)
+            q->f=0;
+    }
+}
+void dequeue(QUEUE *q)
+{
+    if(q->f==-1)
+        printf("\n queue is empty");
+            else
+    {
+        printf("\n element deleted is %d",q->data[q->f]);
+        if(q->f==q->r)
+        {
+            q->f=-1;
+            q->r=-1;
+        }
+        else
+            q->f=q->f+1;
+    }
+}
+    void display(QUEUE q)
+    {
+        int i;
+        if(q.f==-1)
+            printf("\n queue empty");
+        else
+        {
+            printf("\n queue content are\n");
+            for(i=q.f;i<=q.r;i++)
+                printf("%d",q.data[i]);
+        }
+    }
+
+
+
+
+
+int main()
+        {
+
+            int item,ch;
+            QUEUE q;
+            q.f=-1;
+            q.r=-1;
+            for(;;)
+            {
+
+                printf("\n 1.Insert\n");
+                printf("\n 2.Delete\n");
+                printf("\n 3.Display\n");
+                printf("\n 4.Exit\n");
+                printf("\n Read choice:");
+                scanf("%d",&ch);
+                switch(ch)
+                {
+                    case 1:printf("\n Read element to be inserted:");
+                    scanf("%d",&item);
+                    enqueue(&q,item);
+                    break;
+                    case 2:dequeue(&q);
+                    break;
+                    case 3:display(q);
+                    break;
+                    default :exit(0);
+                }
+            }
+            return 0;
+        }
+
+
